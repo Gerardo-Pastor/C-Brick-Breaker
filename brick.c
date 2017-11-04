e3287f98a32a59cf33d093cfdadf0eb6b7792549
Gerardo-Pastor <31077829+Gerardo-Pastor@users.noreply.github.com>
2017-11-04 11:07:14 -0600

    + funciones
    Funcion de pausa (P)
    Deteccion de tamano de pantalla para jugar

--- brick.c
+++ brick.c
@@ -1,108 +1,135 @@
 #include <ncurses.h>
 #include <stdio.h>
 #include <math.h>
-//#include <stdlib.h>
-//#include <unistd.h>
+#include <string.h>
 
 #define DELAY 60000
+void drawBrick(WINDOW *w,int y,int x){
+	mvwprintw(w,y+0,x,"    ____         _        __      ____                      __              ");            
+	mvwprintw(w,y+1,x,"   / __ ) _____ (_)_____ / /__   / __ ) _____ ___   ____ _ / /__ ___   _____");         
+	mvwprintw(w,y+2,x,"  / __  |/ ___// // ___// //_/  / __  |/ ___// _ \\ / __ `// //_// _ \\ / ___/");
+	mvwprintw(w,y+3,x," / /_/ // /   / // /__ /  ,<   / /_/ // /   /  __// /_/ // ,<  /  __// /    ");
+	mvwprintw(w,y+4,x,"/_____//_/   /_/ \\___//_/|_|  /_____//_/    \\___/ \\__,_//_/|_| \\___//_/     ");
+}
+void drawGame(WINDOW *w, int x, int y, int px){
+	wborder( w,'#','#','#','#','#','#','#','#');
+	drawBrick(w,13,7);
+	mvwprintw(w,y, x, "O");
+	mvwprintw(w,28,px-2,"TTTTT");
+}
+void drawPausa(WINDOW *w){
+	mvwprintw(w,7,34,"###   #   # #  ###   #");
+	mvwprintw(w,8,34,"# #  # #  # #  #    # #");
+	mvwprintw(w,9,34,"###  ###  # #  ###  ###");
+	mvwprintw(w,10,34,"#    # #  # #    #  # #");
+	mvwprintw(w,11,34,"#    # #  ###  ###  # #");
+}
 
 int main(int argc, char *argv[]) {
+
 	int x = 2, y = 2;
 	int max_y = 0, max_x = 0;
 	int dirx = 1, diry = 1;
-
-	WINDOW *w;
-	char list[6][10] = { "Jugar", "Facil", "Dificil", "Opciones", "Salir" };
-	char item[10];
-
+	int px=45 , timing = 0;
 	int ch, i;
+
+	WINDOW *w, *v;
+	char list[7][18] = { "Easy", "Medium", "Crazy", "Options*", "Exit","","*out of service" };
+	char item[18];
+
 	initscr();
+
 	getmaxyx(stdscr, max_y, max_x);
-	w = newwin( 10, 15, (max_y/2) - 5, (max_x/2) - 4 ); // create a new window
+	if(max_y < 30 || max_x < 90){
+		v = newwin( max_y, max_x, 0,0);	
+		wtimeout(v,20);
+	   	mvwprintw(v,max_y/3,max_x/2-20,"Screen is too small to play please rezize");
+	   	wrefresh(v);
+	} 
+	else {
+	    v = newwin(9,82,(max_y/4) - 5, (max_x/2)-41);
+	    drawBrick(v,2,3);
+	    wborder( v,'#','#','#','#','#','#','#','#');
+	    wrefresh(v);
+	}
+	w = newwin( 9, 20, (2*max_y/3) - 5, (max_x/2) - 10 ); // create a new window
     box( w, 0, 0 ); // sets default borders for the window
     noecho();
 	curs_set(FALSE);
 	keypad( w, TRUE ); // enable keyboard input for the window.
 
-    for( i=0; i<5; i++ ) {
-        if( i == 0 ) 
-            wattron( w, A_STANDOUT ); 
-        else
-            wattroff( w, A_STANDOUT );
-
-        sprintf(item, "%-10s",  list[i]);
-        mvwprintw( w, i+1, 2, "%s", item );
+    for( i=0; i<7; i++ ) {
+        if( i == 0 ) wattron( w, A_STANDOUT ); 
+        else wattroff( w, A_STANDOUT );
+        sprintf(item, "%s",  list[i]);
+        mvwprintw( w, i+1, 9-((unsigned)strlen(list[i])/2), "%s", item );
     }
- 
-    wrefresh( w );
+    wrefresh(w);
     i = 0;
 	while(( ch = wgetch(w)) != '\n'){  
-            sprintf(item, "%-10s",  list[i]); 
-            mvwprintw( w, i+1, 2, "%s", item ); 
-            switch( ch ) {
-                case KEY_UP:
-                            i--;
-                            i = ( i<0 ) ? 4 : i;
-                            break;
-                case KEY_DOWN:
-                            i++;
-                            i = ( i>4 ) ? 0 : i;
-                            break;
-            }
-            wattron( w, A_STANDOUT );  
-            sprintf(item, "%-10s",  list[i]);
-            mvwprintw( w, i+1, 2, "%s", item);
-            wattroff( w, A_STANDOUT );
+        sprintf(item, "%s",  list[i]); 
+        mvwprintw( w, i+1, 9-((unsigned)strlen(list[i])/2), "%s", item ); 
+        if(ch == KEY_UP && i>0 ) i--;
+        if(ch == KEY_DOWN && i<4 ) i++;
+        wattron( w, A_STANDOUT );  
+        sprintf(item, "%s",  list[i]);
+        mvwprintw( w, i+1, 9-((unsigned)strlen(list[i])/2), "%s", item );
+        wattroff( w, A_STANDOUT );
     }
-    getmaxyx(stdscr, max_y, max_x);
-   	while(max_y < 30 || max_x < 90){
-   		getmaxyx(stdscr, max_y, max_x);
-   		clear();
-   		mvprintw(max_y/2,max_x/2-20,"La pantalla es muy pequena para jugar");
-   		refresh();
-   	}
-   	w = newwin( 30, 90, max_y/2-15,max_x/2-45);
-	keypad( w, TRUE );
-	wtimeout(w,40);
-	int px=45 , timing = 0;
+    wclear(v);
+    delwin(v);
+    ch = 'p';
+	while( ch != 'q'){ 
+	 	getmaxyx(stdscr, max_y, max_x);
+	 	if(max_y < 30 || max_x < 90){
+	 		wclear(w);
+			w = newwin( max_y, max_x, 0,0);	
+			wtimeout(w,20);
+	   		mvwprintw(w,max_y/2,max_x/2-20,"Screen is too small to play please rezize");
+	   		wrefresh(w);
+		}
+		else{
+			wclear(stdscr);
+			w = newwin( 30, 90, max_y/2-15,max_x/2-45);	
+			keypad( w, TRUE );
+			wtimeout(w,20);
+			if(ch == 'p'){
+		   		drawGame(w,x,y,px);
+		   		drawPausa(w);
+		   		wrefresh(w);
+		   		while(( ch = wgetch(w)) != 'p'){}
+			}
+			else{
+				if((ch == KEY_LEFT || ch == 'a')  && px > 4) 	px--;
+				if((ch == KEY_RIGHT || ch == 'd') && px < 86)	px++;
 
- while(( ch = wgetch(w)) != 'q'){ 
-	 if(ch == KEY_LEFT && px > 4) 	px--;
-	 if(ch == KEY_RIGHT && px < 86)	px++;
+				if(timing % 5 == 0){
+				 	if (x > 87 || x < 2) dirx *= -1;
+				 	if (y > 27 || y < 2) diry *= -1;
+				 	if(y==27 && abs(px-x)<3){
+				 	 	dirx-=px-x;
+				 	 	diry *= -1;
+				 	}
+				 	if(dirx > 2)	dirx = 2;
+				 	if(dirx < -2)	dirx = -2;
 
-	 if(timing % 4 == 0){
-	 	 if (x > 87 || x < 2) dirx *= -1;
-	 	 if (y > 27 || y < 2) diry *= -1;
-	 	 if(y==27 && abs(px-x)<3){
-	 	 	dirx-=px-x;
-	 	 	diry *= -1;
-	 	 }
-	 	 if(dirx > 2)	dirx = 2;
-	 	 if(dirx < -2)	dirx = -2;
-
-		 x += dirx;
-		 y += diry;
-		 timing = 0;
-	 }
-	 if(ch == 'p'){
- 	 	wtimeout(w,-1);
- 	 	wgetch(w);
- 	 	wtimeout(w,50);
- 	 }
- 	 wclear(w);
- 	 wborder( w,'#','#','#','#','#','#','#','#');
- 	 mvwprintw(w,1,5," px: %d key: %c x: %d y: %d", px, ch, x, y );
-	 mvwprintw(w,13,7,"    ____         _        __      ____                      __              ");            
- 	 mvwprintw(w,14,7,"   / __ ) _____ (_)_____ / /__   / __ ) _____ ___   ____ _ / /__ ___   _____");         
-	 mvwprintw(w,15,7,"  / __  |/ ___// // ___// //_/  / __  |/ ___// _ \\ / __ `// //_// _ \\ / ___/");
-	 mvwprintw(w,16,7," / /_/ // /   / // /__ /  ,<   / /_/ // /   /  __// /_/ // ,<  /  __// /    ");
-	 mvwprintw(w,17,7,"/_____//_/   /_/ \\___//_/|_|  /_____//_/    \\___/ \\__,_//_/|_| \\___//_/     ");
-	 mvwprintw(w,y, x, "O");
-	 mvwprintw(w,28,px-2,"TTTTT");
- 	 wrefresh(w);
- 	 timing++;
- }
- clear();
- endwin();
- return 0;
+					x += dirx;
+					y += diry;
+					timing = 0;
+				}
+				if(ch == 'p'){
+			 	 	wtimeout(w,-1);
+			 	 	wgetch(w);
+			 	 	wtimeout(w,50);
+			 	}
+			}
+			mvwprintw(w,1,5," px: %d key: %c max_x: %d max_y: %d", px, ch, max_x, max_y );
+			drawGame(w,x,y,px);
+			timing++;
+		}
+		ch = wgetch(w);
+	}
+	clear();
+	endwin();
+	return 0;
 }
